e1b4ab308bfdeb5355bbbd921572fa7043731214
MorenoNegrito <negrito.pankeke@gmailcom>
2025-10-22 17:13:48 -0300

    feat: implementar sistema de navegación con rutas y eventos

---
+++ app/src/main/java/com/example/koltin/navigation/AppRoutes.kt
@@ -0,0 +1,17 @@
+package com.example.koltin.navigation
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("Home_page")
+    data object profile : Screen("Profile_page")
+    data object setting : Screen("setting_Page")
+    data object Registro : Screen("Registro_page")
+    data object Resumen : Screen("Resumen_page")
+    data object Mascotas : Screen("Mascotas_page")
+    data object ReservarHora : Screen("ReservarHora_page")
+
+    data class Detail(val itemId: String) : Screen("detail_page/{itemId}") {
+        fun buildRoute(): String {
+            return route.replace("{itemId}", itemId)
+        }
+    }
+}
