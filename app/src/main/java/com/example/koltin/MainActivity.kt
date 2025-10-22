4f6b1b9ec0510fadc2f40f9a924427dc68dd8162
MorenoNegrito <negrito.pankeke@gmailcom>
2025-10-22 17:52:00 -0300

    feat: integrar navegación inferior en pantalla principal

--- app/src/main/java/com/example/koltin/MainActivity.kt
+++ app/src/main/java/com/example/koltin/MainActivity.kt
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.koltin.ui.theme.KoltinTheme
+import com.example.koltin.ui.theme.screen.HomeScreen
+import com.example.koltin.ui.theme.screen.HomeScreenWithNavBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             KoltinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreenWithNavBar(navController = rememberNavController())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KoltinTheme {
-        Greeting("Android")
     }
 }
